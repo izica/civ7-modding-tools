9298e273023ec17eaba2f36ee6e049fb2cc9bdaf
izica <artemiztomska@gmail.com>
2025-03-16 03:50:21 +0700

    great person progress

--- src/builders/GreatPersonBuilder.ts
+++ src/builders/GreatPersonBuilder.ts
@@ -1,25 +1,46 @@
 import { kebabCase } from "lodash";
 
 import { TClassProperties, TPartialRequired } from "../types";
-import { DatabaseNode, TIconDefinitionNode, TUnitNode } from "../nodes";
-import { ACTION_GROUP_ACTION } from "../constants";
+import {
+    DatabaseNode, GameEffectNode,
+    GreatPersonClassNode,
+    GreatPersonIndividualNode,
+    TGreatPersonClassNode,
+    TGreatPersonIndividualActionModifierNode,
+    TGreatPersonIndividualNode,
+    TIconDefinitionNode,
+    TModifierNode,
+    TypeNode
+} from "../nodes";
+import { ACTION_GROUP_ACTION, KIND } from "../constants";
 import { XmlFile } from "../files";
 
 import { BaseBuilder } from "./BaseBuilder";
+import { locale } from "../utils";
 
 type TUnlockRewardBuilder = TClassProperties<GreatPersonBuilder>;
 
 export class GreatPersonBuilder extends BaseBuilder<TUnlockRewardBuilder> {
     _current: DatabaseNode = new DatabaseNode();
+    _gameEffects: GameEffectNode = new GameEffectNode();
     _shell: DatabaseNode = new DatabaseNode();
     _icons: DatabaseNode = new DatabaseNode();
     _localizations: DatabaseNode = new DatabaseNode();
 
-    type = 'LOGIOS';
+    alias = 'LOGIOS';
 
     icon: TPartialRequired<TIconDefinitionNode, 'path'> = {
         path: 'fs://game/civ_sym_han'
     }
+
+    greatPersonClass: Partial<TGreatPersonClassNode> = {};
+
+    greatPersonIndividuals: (Partial<TGreatPersonIndividualNode> & {
+        alias: string,
+        greatPersonIndividualActionModifiers: (TPartialRequired<TGreatPersonIndividualActionModifierNode, 'attachmentTargetType'> & {
+            modifier: Partial<TModifierNode>
+        })[]
+    })[] = [];
 
     constructor(payload: Partial<TUnlockRewardBuilder> = {}) {
         super();
@@ -27,17 +48,55 @@ export class GreatPersonBuilder extends BaseBuilder<TUnlockRewardBuilder> {
     }
 
     migrate() {
+        const unitType = `UNIT_${this.alias}`;
+        const greatPersonClassType = `GREAT_PERSON_CLASS_${this.alias}`;
+
+        this._current.fill({
+            types: [new TypeNode({ type: greatPersonClassType, kind: KIND.GREAT_PERSON_CLASS })],
+            greatPersonClasses: [
+                new GreatPersonClassNode({
+                    greatPersonClassType,
+                    unitType,
+                    name: locale(unitType, 'name'),
+                    ...this.greatPersonClass
+                })
+            ]
+        });
+
+        this.greatPersonIndividuals.forEach(item => {
+            const greatPersonIndividualType = `GREAT_PERSON_INDIVIDUAL_${item.alias}`;
+
+            this._current.types.push(new TypeNode({ type: greatPersonIndividualType, kind: KIND.GREAT_PERSON_INDIVIDUAL }));
+            this._current.greatPersonIndividuals.push(
+                new GreatPersonIndividualNode({
+                    greatPersonIndividualType,
+                    greatPersonClassType,
+                    unitType,
+                    name: locale(greatPersonIndividualType, 'name'),
+                    actionEffectTextOverride: locale(greatPersonIndividualType, 'description'),
+                    actionNameTextOverride: locale(greatPersonIndividualType, 'retire'),
+                    ...item
+                })
+            );
+        });
 
         return this;
     }
 
     build() {
-        const path = `/great-persons/${kebabCase('')}/`;
+        const path = `/great-persons/${kebabCase(this.alias)}/`;
         return [
             new XmlFile({
                 path,
                 name: 'current.xml',
                 content: this._current.toXmlElement(),
+                actionGroups: [this.actionGroupBundle.current],
+                actionGroupActions: [ACTION_GROUP_ACTION.UPDATE_DATABASE]
+            }),
+            new XmlFile({
+                path,
+                name: 'game-effects.xml',
+                content: this._gameEffects.toXmlElement(),
                 actionGroups: [this.actionGroupBundle.current],
                 actionGroupActions: [ACTION_GROUP_ACTION.UPDATE_DATABASE]
             }),
--- src/nodes/GreatPersonClassNode.ts
+++ src/nodes/GreatPersonClassNode.ts
@@ -5,9 +5,11 @@ export type TGreatPersonClassNode = Pick<GreatPersonClassNode,
     "name" |
     "unitType" |
     "uniqueQuarterType" |
+    "constructibleType" |
     "availableInTimeline" |
     "generateDuplicateIndividuals" |
     "pseudoYieldType" |
+    "populationRequired" |
     "iconString" |
     "actionIcon"
 >;
@@ -17,9 +19,11 @@ export class GreatPersonClassNode extends BaseNode<TGreatPersonClassNode> {
     name: string | null = null;
     unitType: string | null = null;
     uniqueQuarterType: string | null = null;
+    constructibleType: string | null = null;
     availableInTimeline: boolean | null = null;
     generateDuplicateIndividuals: boolean | null = null;
     pseudoYieldType: string | null = null;
+    populationRequired: number | null = null;
     iconString: string | null = 'unitflag_logios';
     actionIcon: string | null = 'action_greatperson';
 
