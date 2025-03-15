12419b4b6c667450506860b2df5ce869d2ece386
izica <artemiztomska@gmail.com>
2025-03-15 23:36:04 +0700

    Update unlock-builder.ts

--- examples/unlock-builder.ts
+++ examples/unlock-builder.ts
@@ -1,5 +1,7 @@
 import { REQUIREMENT, REQUIREMENT_SET, RESOURCE, UnlockBuilder } from "./src";
+import { ACTION_GROUP_BUNDLE, AGE, CIVILIZATION_DOMAIN } from "../src";
 
+// unlock by 3 improved iron
 const civilizationUnlock = new UnlockBuilder({
     unlockConfigurationValue: {
         configurationValue: civilization.civilization.civilizationType
@@ -13,5 +15,28 @@ const civilizationUnlock = new UnlockBuilder({
                 { name: 'ResourceType', value: RESOURCE.IRON }
             ]
         }]
+    }
+});
+
+// unlock exploration civ from antiquity greece
+const civilizationFromGreece = new UnlockBuilder({
+    name: 'from Greece',
+    actionGroupBundle: ACTION_GROUP_BUNDLE.AGE_EXPLORATION,
+    civilizationUnlock: {
+        civilizationDomain: CIVILIZATION_DOMAIN.AntiquityAgeCivilizations,
+        civilizationType: 'CIVILIZATION_GREECE',
+        ageType: AGE.EXPLORATION,
+        type: civilization.civilization.civilizationType
+    }
+});
+
+// unlock exploration civ from leader catherine
+const leaderCatherineUnlock = new UnlockBuilder({
+    name: 'catherine',
+    actionGroupBundle: ACTION_GROUP_BUNDLE.AGE_EXPLORATION,
+    leaderUnlock: {
+        leaderType: 'LEADER_CATHERINE',
+        ageType: AGE.EXPLORATION,
+        type: civilization.civilization.civilizationType
     }
 });
