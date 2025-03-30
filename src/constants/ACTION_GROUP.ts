3b4d3713f3346d0249c0b44042e6602828ebe246
James Hill <me@jameshill.io>
2025-03-30 17:26:26 +1300

    Fix ACTION_GROUP so jsdoc is preserved in compile

--- src/constants/ACTION_GROUP.ts
+++ src/constants/ACTION_GROUP.ts
@@ -6,7 +6,34 @@ import { AGE } from "./AGE";
 
 // NB: IDs on ActionGroups and CriteriaNodes for the ages are set to follow  Firaxis'
 // conventions present the main game files in e.g. Base\modules\age-antiquity\age-antiquity.modinfo.
-const actionGroupInternal = {
+
+
+const ageAntiquityPersist = new ActionGroupNode({
+    id: `age-antiquity-persist`,
+    scope: 'game',
+    criteria: new CriteriaNode({
+        id: `antiquity-age-persist`,
+        ages: [AGE.ANTIQUITY, AGE.EXPLORATION, AGE.MODERN]
+    })
+})
+const ageExplorationPersist = new ActionGroupNode({
+    id: `age-exploration-persist`,
+    scope: 'game',
+    criteria: new CriteriaNode({
+        id: `exploration-age-persist`,
+        ages: [AGE.EXPLORATION, AGE.MODERN]
+    })
+})
+const ageModernPersist = new ActionGroupNode({
+    id: `age-modern-persist`,
+    scope: 'game',
+    criteria: new CriteriaNode({
+        id: `modern-age-persist`,
+        ages: [AGE.MODERN]
+    })
+})
+
+export const ACTION_GROUP = {
     SHELL: new ActionGroupNode({
         scope: 'shell',
         criteria: new CriteriaNode({ id: 'always' })
@@ -23,14 +50,7 @@ const actionGroupInternal = {
             ages: [AGE.ANTIQUITY]
         })
     }),
-    AGE_ANTIQUITY_PERSIST: new ActionGroupNode({
-        id: `age-antiquity-persist`,
-        scope: 'game',
-        criteria: new CriteriaNode({
-            id: `antiquity-age-persist`,
-            ages: [AGE.ANTIQUITY, AGE.EXPLORATION, AGE.MODERN]
-        })
-    }),
+    AGE_ANTIQUITY_PERSIST: ageAntiquityPersist,
     AGE_EXPLORATION_CURRENT: new ActionGroupNode({
         id: `age-exploration-current`,
         scope: 'game',
@@ -39,14 +59,7 @@ const actionGroupInternal = {
             ages: [AGE.EXPLORATION]
         })
     }),
-    AGE_EXPLORATION_PERSIST: new ActionGroupNode({
-        id: `age-exploration-persist`,
-        scope: 'game',
-        criteria: new CriteriaNode({
-            id: `exploration-age-persist`,
-            ages: [AGE.EXPLORATION, AGE.MODERN]
-        })
-    }),
+    AGE_EXPLORATION_PERSIST: ageExplorationPersist,
     AGE_MODERN_CURRENT: new ActionGroupNode({
         id: `age-modern-current`,
         scope: 'game',
@@ -55,33 +68,21 @@ const actionGroupInternal = {
             ages: [AGE.MODERN]
         })
     }),
-    AGE_MODERN_PERSIST: new ActionGroupNode({
-        id: `age-modern-persist`,
-        scope: 'game',
-        criteria: new CriteriaNode({
-            id: `modern-age-persist`,
-            ages: [AGE.MODERN]
-        })
-    }),
-};
+    AGE_MODERN_PERSIST: ageModernPersist,
 
-const actionGroupDeprecatedProps = {
     /**
-     * @deprecated Use AGE_ANTIQUITY_PERSIST directly instead. This may be removed in a future release
+     * @deprecated Use AGE_ANTIQUITY_PERSIST directly instead. Preserved for backwards
+     * compatibility and may be removed in a future release
      */
-    AGE_ANTIQUITY_EXIST: actionGroupInternal.AGE_ANTIQUITY_PERSIST,
+    AGE_ANTIQUITY_EXIST: ageAntiquityPersist,
     /**
-     * @deprecated Use AGE_EXPLORATION_PERSIST directly instead. This may be removed in a future release
+     * @deprecated Use AGE_EXPLORATION_PERSIST directly instead. Preserved for backwards
+     * compatibility and may be removed in a future release
      */
-    AGE_EXPLORATION_EXIST: actionGroupInternal.AGE_EXPLORATION_PERSIST,
+    AGE_EXPLORATION_EXIST: ageExplorationPersist,
     /**
-     * @deprecated Use AGE_MODERN_PERSIST directly instead. This may be removed in a future release
+     * @deprecated Use AGE_MODERN_PERSIST directly instead. Preserved for backwards
+     * compatibility and may be removed in a future release
      */
-    AGE_MODERN_EXIST: actionGroupInternal.AGE_MODERN_PERSIST,
-};
-
-export const ACTION_GROUP = {
-    ...actionGroupInternal,
-    ...actionGroupDeprecatedProps
+    AGE_MODERN_EXIST: ageModernPersist,
 } as const;
-
